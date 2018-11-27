b65cf599e4d9c4174012d4150a714eafb547a442
Mai Schwartz <mai.schwartz@gmail.com>
2018-11-27 22:52:40 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,10 +1,32 @@
-def display_board(board)
+def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
 
 end
 
 def valid_move?(board, index)
-
+  valid = false
+  taken = position_taken?(board, index)
+  if (index.between?(0,8) && taken == false)
+    valid = true
+  end
+valid
 end
+
+def position_taken?(board, index)
+  taken = nil
+  if (board[index] == "X" || board[index] == "O")
+    taken = true
+  else
+    taken = false
+  end
+taken
+end
+
 
 def move(board, index, user = "X")
 
